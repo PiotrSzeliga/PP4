73a8110b840807e418e36b0e66f054aab784ba53
Piotr Szeliga <piotrszeliga87@gmail.com>
2025-03-16 23:36:04 +0100

    Add project files.

---
+++ EShopService/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace EShopService.Models
+{
+    public class Product : BaseModel
+    {
+        public string Ean {  get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Stock { get; set; } = 0;
+        public string Sku { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+    }
+}
