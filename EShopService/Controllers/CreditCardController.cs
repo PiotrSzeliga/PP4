a1e1a59e81b1dc075e879454d1ac1349566fe28a
PiotrSzeliga <piotrszeliga87@gmail.com>
2025-04-28 13:39:46 +0200

    kskhfzkjhkjvsb

--- EShopService/Controllers/CreditCardController.cs
+++ EShopService/Controllers/CreditCardController.cs
@@ -2,43 +2,42 @@
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace EShopService.Controllers
+namespace EShopService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CreditCardController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CreditCardController : ControllerBase
+
+    // GET: api/<CreditCardController>
+    [HttpGet]
+    public IEnumerable<string> Get()
     {
+        return new string[] { "value1", "value2" };
+    }
 
-        // GET: api/<CreditCardController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+    // GET api/<CreditCardController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
 
-        // GET api/<CreditCardController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+    // POST api/<CreditCardController>
+    [HttpPost]
+    public void Post([FromBody]string value)
+    {
+    }
 
-        // POST api/<CreditCardController>
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
+    // PUT api/<CreditCardController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody]string value)
+    {
+    }
 
-        // PUT api/<CreditCardController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<CreditCardController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+    // DELETE api/<CreditCardController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
     }
 }
